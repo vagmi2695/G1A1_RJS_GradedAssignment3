c5526d9b938d9a7476d9276d48b20874c8fae1d4
vagmi2695 <56200242+vagmi2695@users.noreply.github.com>
2023-06-01 12:31:38 +0530

    Add files via upload

---
+++ movie-on-tip/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { ToastContainer } from "react-toastify"
+
+
+import Navbar from "./component/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
